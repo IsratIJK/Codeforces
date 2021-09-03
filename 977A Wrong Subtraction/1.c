acb9c465793eb7f48063cd9b21125d17da4c4ed5
IsratIJK <isratjahankhanijk@gmail.com>
2021-09-03 19:52:42 +0600

    solved

---
+++ 977A Wrong Subtraction/1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i,n,k;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<k;i++){
+        if(n%10!=0){
+            n--;
+        }
+        else if(n%10==0){
+            n=n/10;
+        }
+    }
+    printf("%d\n",n);
+    
+}
