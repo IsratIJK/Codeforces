e308028febc366cb6fa443014003e623617ae8db
IsratIJK <isratjahankhanijk@gmail.com>
2021-09-06 19:27:40 +0600

    solved

--- 96A Football/1.c
+++ 96A Football/1.c
@@ -1,29 +1,14 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-    int i,flag;
+    
     char arr[100];
     scanf("%s",&arr);
-    for(i=0;arr[i]!='\0';i++){
-        if(arr[i]==0 && arr[i+1]==0 && arr[i+2]==0 && arr[i+3]==0 && arr[i+4]==0 && arr[i+5]==0 && arr[i]+6==0){
-            flag=0;
-            break;
-            
-        }
-        if(arr[i]==1 && arr[i+1]==1 && arr[i+2]==1 && arr[i+3]==1 && arr[i+4]==1 && arr[i+5]==1 && arr[i]+6==1){
-            flag=0;
-            break;
-        }
-       
-
+    if((strstr(arr, "0000000")) || (strstr(arr, "1111111"))){
+        printf("YES\n");
     }
-     if(flag==0){
-            printf("YES\n");
-
-        }
-
-        else if(flag==1){
-            printf("NO\n");
-        }
-
+    else{
+       printf("NO\n"); 
+    }
 
 }
